2f7485e2c1e653ad9a4767a32264d25e4f37bc2c
regen31 <rcly31@gmail.com>
2017-08-12 17:07:35 +0300

    many classes and methods added

---
+++ go fish/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace go_fish
+{
+    class Card
+    {
+        public Suits Suit;
+        public Values Value;
+        public string Name { get { return Value.ToString() + " of " + Suit.ToString(); } }
+
+        public Card (Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public static string Plural (Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+    }
+}
---
+++ go fish/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace go_fish
+{
+    class Player
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private Deck cards;
+        private TextBox textBoxOnForm;
+
+        public Player (String name, Random random, TextBox textBoxOnForm)
+        {
+            this.name = name;
+            this.random = random;
+            this.cards = new Deck(new Card[] { });
+            textBoxOnForm.Text += name + " has just joined the game" + Environment.NewLine;
+        }
+
+        public IEnumerable<Values> PullOutOfBooks() { }
+
+        public Values GetRandomValue()
+        {
+            Card randomCard = cards.Peek(random.Next(cards.Count));
+            return randomCard.Value;    
+        }
+
+        public Deck DoYouHaveAny(Values value)
+        {
+            Deck cardsIHave = cards.PullOutValues(value);
+            textBoxOnForm.Text += Name + " has " + cardsIHave.Count + " " + Card.Plural(value) + Environment.NewLine;
+            return cardsIHave;
+        }
+    }
+}
